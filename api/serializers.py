bed45ad8f94d50e1a6e28409fb03141416f69d7e
healingfields <dihratene@gmail.com>
2022-03-19 00:13:41 +0100

    added platform serializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,18 +1,23 @@
 from logging import exception
 from wsgiref.validate import validator
 from rest_framework import serializers
-from djJson.models import Movie
+from djJson.models import WatchList, StreamPlatform
 from rest_framework import validators
 
-class MovieSerializer(serializers.ModelSerializer):
+class StreamPlatformSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = StreamPlatform
+        fields = '__all__'
+
+class WatchListSerializer(serializers.ModelSerializer):
     
     name_length = serializers.SerializerMethodField()
     
     class Meta:
-        model = Movie
+        model = WatchList
         fields = "__all__"
         # exclude = ['active']
-        
+
     def get_name_length(self, object):
         return len(object.name)
 
