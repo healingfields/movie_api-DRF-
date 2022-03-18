b06a9538d5942b59c02e4442e5796481987bbd9f
healingfields <dihratene@gmail.com>
2022-03-18 18:27:15 +0100

    added validation to serializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,10 +1,17 @@
+from wsgiref.validate import validator
 from rest_framework import serializers
 from djJson.models import Movie
+from rest_framework import validators
+
+def check_length(value):
+    if len(value)<3:
+        raise validators.ValidationError('name must contains more than 3 caracteres')
+    return value
 
 class MovieSerializer(serializers.Serializer):
 
     id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField(max_length=150)
+    name = serializers.CharField(max_length=150, validators=[check_length])
     description = serializers.CharField(max_length=250)
     active = serializers.BooleanField()
     
@@ -17,3 +24,14 @@ class MovieSerializer(serializers.Serializer):
         instance.active = validated_data.get('active', instance.active)
         instance.save()
         return instance
+    
+    def validate(self, data):
+        if data['name'] == data['description']:
+            raise validators.ValidationError('name and description should not be the same')
+        return data
+    
+    # def validate_name(self, value):
+    #     if len(value)<2:
+    #         raise validators.ValidationError('name must contains more than 2 caracteres')
+    #     return value
+            
