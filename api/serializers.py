d03381284af5b2a51c033ce5d6f8f7086c5c7c6d
healingfields <dihratene@gmail.com>
2022-03-18 18:43:55 +0100

    added method field to serializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,34 +1,47 @@
+from logging import exception
 from wsgiref.validate import validator
 from rest_framework import serializers
 from djJson.models import Movie
 from rest_framework import validators
 
-def check_length(value):
-    if len(value)<3:
-        raise validators.ValidationError('name must contains more than 3 caracteres')
-    return value
+class MovieSerializer(serializers.ModelSerializer):
+    
+    name_length = serializers.SerializerMethodField()
+    
+    class Meta:
+        model = Movie
+        fields = "__all__"
+        # exclude = ['active']
+        
+    def get_name_length(self, object):
+        return len(object.name)
 
-class MovieSerializer(serializers.Serializer):
+# def check_length(value):
+#     if len(value)<3:
+#         raise validators.ValidationError('name must contains more than 3 caracteres')
+#     return value
 
-    id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField(max_length=150, validators=[check_length])
-    description = serializers.CharField(max_length=250)
-    active = serializers.BooleanField()
+# class MovieSerializer(serializers.Serializer):
+
+#     id = serializers.IntegerField(read_only=True)
+#     name = serializers.CharField(max_length=150, validators=[check_length])
+#     description = serializers.CharField(max_length=250)
+#     active = serializers.BooleanField()
     
-    def create(self, validated_date):
-        return Movie.objects.create(**validated_date)
+#     def create(self, validated_date):
+#         return Movie.objects.create(**validated_date)
     
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get('name', instance.name)
-        instance.description = validated_data.get('description', instance.description)
-        instance.active = validated_data.get('active', instance.active)
-        instance.save()
-        return instance
+#     def update(self, instance, validated_data):
+#         instance.name = validated_data.get('name', instance.name)
+#         instance.description = validated_data.get('description', instance.description)
+#         instance.active = validated_data.get('active', instance.active)
+#         instance.save()
+#         return instance
     
-    def validate(self, data):
-        if data['name'] == data['description']:
-            raise validators.ValidationError('name and description should not be the same')
-        return data
+#     def validate(self, data):
+#         if data['name'] == data['description']:
+#             raise validators.ValidationError('name and description should not be the same')
+#         return data
     
     # def validate_name(self, value):
     #     if len(value)<2:
