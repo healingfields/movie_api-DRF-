5599aff18139cceda1957f9ebf1ca8f556690510
healingfields <dihratene@gmail.com>
2022-03-19 00:11:24 +0100

    changed my models

--- djJson/models.py
+++ djJson/models.py
@@ -1,10 +1,18 @@
 from django.db import models
 
-
-class Movie(models.Model):
+class StreamPlatform(models.Model):
     name = models.CharField(max_length=150)
-    description = models.TextField(max_length=250)
+    about = models.CharField(max_length=250)
+    website = models.URLField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class WatchList(models.Model):
+    name = models.CharField(max_length=150)
+    storyline = models.CharField(max_length=250)
     active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
         return self.name
