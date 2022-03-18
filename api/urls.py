ef6ed556931d1b5331dee1ca2f9d163e3befba66
healingfields <dihratene@gmail.com>
2022-03-18 17:40:24 +0100

    movie crud using APIVIEW

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import movie_list, movie_details
+# from .views import movie_list, movie_details, MovieListAV
+from .views import MovieListAV, MovieDetailAV
 
 urlpatterns = [
-    path('movies/', movie_list),
-    path('movies/<int:pk>/', movie_details),
+    # path('movies/', movie_list),
+    # path('movies/<int:pk>/', movie_details),
+    path('movies/', MovieListAV.as_view()),
+    path('movies/<int:pk>/', MovieDetailAV.as_view()),
+    
 ]
