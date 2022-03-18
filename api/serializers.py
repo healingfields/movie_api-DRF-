f16ce3c37d19693ae88e966337327de6b2ff06d4
healingfields <dihratene@gmail.com>
2022-03-18 16:41:19 +0100

    added create and update method to movie serializer

--- api/serializers.py
+++ api/serializers.py
@@ -7,4 +7,13 @@ class MovieSerializer(serializers.Serializer):
     name = serializers.CharField(max_length=150)
     description = serializers.CharField(max_length=250)
     active = serializers.BooleanField()
-
+    
+    def create(self, validated_date):
+        return Movie.objects.create(**validated_date)
+    
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.description = validated_data.get('description', instance.description)
+        instance.active = validated_data.get('active', instance.active)
+        instance.save()
+        return instance
