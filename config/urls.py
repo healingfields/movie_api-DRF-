e8aef155b637cfb724d48b35264dcc35c3570ec0
healingfields <dihratene@gmail.com>
2022-03-24 19:48:06 +0100

    added authentication

--- config/urls.py
+++ config/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('json/', include('djJson.urls')),
-    path('watch/', include('api.urls'))
+    path('watch/', include('api.urls')),
+    path('api-auth/', include('rest_framework.urls'))
 
 ]
