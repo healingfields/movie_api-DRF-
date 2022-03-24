14c6dd45072670fd6c69bdee9ea915692c32db2a
healingfields <dihratene@gmail.com>
2022-03-24 17:48:12 +0100

    added endpoints for the previous views

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 # from .views import movie_list, movie_details, MovieListAV
-from .views import WatchListListAV, StreamPlatformListAV, WatchListDetailAV, StreamPlatformDetailAV, ReviewList, ReviewDetail
+from .views import WatchListListAV, StreamPlatformListAV, WatchListDetailAV, StreamPlatformDetailAV, ReviewListByWatchlist, ReviewDetail, ReviewCreateByWatchlist
 
 urlpatterns = [
     # path('movies/', movie_list),
@@ -9,6 +9,10 @@ urlpatterns = [
     path('watchlists/<int:pk>/', WatchListDetailAV.as_view(), name='watchlist-detail'),
     path('platforms/', StreamPlatformListAV.as_view()),
     path('platforms/<int:pk>/', StreamPlatformDetailAV.as_view(), name='streamplatform-detail'),
-    path('review/', ReviewList.as_view(), name='review-list'),
+
+    path('watchlist/<int:pk>/review-create/', ReviewCreateByWatchlist.as_view()),
+    path('watchlist/<int:pk>/review/', ReviewListByWatchlist.as_view(), name='review-list'),
     path('review/<int:pk>/', ReviewDetail.as_view(), name='review-detail')
+
+
 ]
