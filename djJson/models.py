7e019f2b110d7be30bc582d29f859a86481cfbef
healingfields <dihratene@gmail.com>
2022-03-25 23:45:47 +0100

    added 2 fields to Watchlist model and migrated

--- djJson/models.py
+++ djJson/models.py
@@ -12,16 +12,20 @@ class StreamPlatform(models.Model):
     def __str__(self):
         return self.name
 
+
 class WatchList(models.Model):
 
     name = models.CharField(max_length=150)
     storyline = models.CharField(max_length=250)
     active = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
+    avg_rating = models.FloatField(default=0)
+    nbr_reviews = models.PositiveIntegerField(default=0)
     platform = models.ForeignKey(StreamPlatform, related_name='watchlists', on_delete=models.CASCADE)
     
     def __str__(self):
         return self.name
+
 
 class Review(models.Model):
 
