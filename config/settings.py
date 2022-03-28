f56947abe6aeee8fd0fc45f47f7c2674a2fcc99f
healingfields <dihratene@gmail.com>
2022-03-28 16:38:23 +0000

    adding both basic and token authentication

--- config/settings.py
+++ config/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'djJson',
-    'api'
+    'api',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -126,8 +127,15 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # REST_FRAMEWORK = {
+
+# }
+REST_FRAMEWORK = {
 #     'DEFAULT_PERMISSION_CLASSES':[
 #         'rest_framework.permissions.IsAuthenticated',
 #     ]
-# }
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        # 'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ]
+}
 
--- config/urls.py
+++ config/urls.py
@@ -20,6 +20,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('json/', include('djJson.urls')),
     path('watch/', include('api.urls')),
-    path('api-auth/', include('rest_framework.urls'))
+    # path('api-auth/', include('rest_framework.urls'))
 
 ]
