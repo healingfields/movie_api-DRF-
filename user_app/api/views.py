3868f892f21ecee05e6029582eea1de09396bcd5
healingfields <dihratene@gmail.com>
2022-04-05 14:54:27 +0000

    configuring jwt token

--- user_app/api/views.py
+++ user_app/api/views.py
@@ -3,7 +3,8 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import  status
 from rest_framework.authtoken.models import Token
-from . import models
+# from . import models
+from rest_framework_simplejwt.tokens import RefreshToken
 
 
 @api_view(['POST'])
@@ -16,8 +17,13 @@ def register_view(request):
             account = serializer.save()
             data['username'] = account.username
             data['email'] = account.email
-            token = Token.objects.get(user=account).key
-            data['token'] = token
+            # token = Token.objects.get(user=account).key
+            # data['token'] = token
+            refresh = RefreshToken.for_user(account)
+            data['token'] = {
+                'refresh': str(refresh),
+                'access': str(refresh.access_token),
+            }
             return Response(data)
         return Response(serializer.errors)
 
